7c5a615b7a616e4f30a4cffbeea0e3cd29ffba4a
Thivstan James <114639488+thivistan@users.noreply.github.com>
2025-07-12 15:01:03 -0500

    Add folder structure and placeholders

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import RoommateForm from './components/RoommateForm';
+
+export default function App() {
+  return (
+    <div className="App">
+      <RoommateForm />
+    </div>
+  );
+}
---
+++ frontend/src/components/RoommateForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function RoommateForm() {
+  return (
+    <form>
+      {/* TODO: implement form fields */}
+      <button type="submit">Generate Vibe Profile</button>
+    </form>
+  );
+}
